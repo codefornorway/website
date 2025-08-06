aaf465732e80d0021b7104d804785a6e7332f351
Sefa Bulak <zckhtln@icloud.com>
2025-08-06 18:18:04 +0200

    feat(ui): switch to Nuxt UI components instead of Tailwind UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  modules: ['@nuxt/ui'],
   devtools: { enabled: true },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  css: ['@/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
   future: { compatibilityVersion: 4 },
+  ui: {
+    colorMode: false,
+  },
 });
